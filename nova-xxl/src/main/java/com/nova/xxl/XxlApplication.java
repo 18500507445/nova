2aa2aea82c297af7ed4f192c8d81f286fd746b5a
wangzehui <1251850055@qq.com>
2022-06-19 11:47:35 +0800

    git忽略

---
+++ nova-xxl/src/main/java/com/nova/xxl/XxlApplication.java
@@ -0,0 +1,16 @@
+package com.nova.xxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangzehui
+ */
+@SpringBootApplication
+public class XxlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlApplication.class, args);
+    }
+
+}
---
+++ nova-xxl/src/test/java/com/nova/xxl/XxlApplicationTest.java
@@ -0,0 +1,14 @@
+package com.nova.xxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}

5dd386d48205844a0e72757d24248df0eb96a03b
wangzehui <18500507445@163.com>
2022-08-29 21:47:53 +0800

    支付改造

--- nova-pay/src/main/java/com/nova/pay/config/YeePayConfig.java
+++ nova-pay/src/main/java/com/nova/pay/config/YeePayConfig.java
@@ -10,17 +10,17 @@ public class YeePayConfig {
     /**
      * app_key:app_10088528644
      */
-    public static final String APP_KEY = "app_10085537650";
+    public static final String APP_KEY = "";
 
     /**
      * 易宝公钥
      */
-    public static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6p0XWjscY+gsyqKRhw9MeLsEmhFdBRhT2emOck/F1Omw38ZWhJxh9kDfs5HzFJMrVozgU+SJFDONxs8UB0wMILKRmqfLcfClG9MyCNuJkkfm0HFQv1hRGdOvZPXj3Bckuwa7FrEXBRYUhK7vJ40afumspthmse6bs6mZxNn/mALZ2X07uznOrrc2rk41Y2HftduxZw6T4EmtWuN2x4CZ8gwSyPAW5ZzZJLQ6tZDojBK4GZTAGhnn3bg5bBsBlw2+FLkCQBuDsJVsFPiGh/b6K/+zGTvWyUcu+LUj2MejYQELDO3i2vQXVDk7lVi2/TcUYefvIcssnzsfCfjaorxsuwIDAQAB";
+    public static final String PUBLIC_KEY = "";
 
     /**
      * 商户私钥
      */
-    public static final String PRIVATE_KEY = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCQHyXI/etxIIknhBQzgTjKuanlnAAyNH7Dmfwf3yvpW6icPTBAyM1kWw2Tj3s23tBI1JPFn3ZlRUEbe5Oa6i0/n6GvFfgPViWR+KTxeYTm+KzjPnrFo5GKDbheiuxxmSDa6YH2FkvwGlUPLvmDGGBfdOA/PtN42UBARAs5wxh3JDxNWJ8ksd6o+YQN55Qzqw91uOsvNu/gerQaZsY1O6ROZg3d3S6BEu2b4mp3Q4Lvw81+BD43hhxUfOZz6sdxfimNItN5NBMSvf84tc8CSN90hMAlf0y+Akngw65edRgY/FWd8HYmVuAhz/6AUI2S/nqQ+4Oi1lQZRYHTOv7eKxE/AgMBAAECggEAJMKNFLMPdo9h7F5X+LshDTRGDXa45hVVr/Z6xHZb8O2cGB3TTPLq0IoY8GZMezZh0fHGr0V5IWOEs8nVYFISsIO67bmgx+ilNJJDIARqza7oCh5JmleaK6hMOnEc1ezE4JFmwKn3PIq632CP68wv6Kka+YbA0bY4GSxB4L3OY7TMQrEFt4170YIDmIqdIVYzpoL4ngEGsaXRAuV7v3AchqZHjgpG0F8fYFdWBrGxhYN7k3EIBMI6XxZ0nX0eA3Lfln+8gPgOiezl4AHM4mznjn/lk7yzAwe0hrVyBBpp6xK4EqEu3B2dgUDrCljqK5gj8OCa2VK+7+o3O6pLyObngQKBgQDJnmZqDngCvPOBFgcwy0DqU4nbpkIbrF3IB9Ak+fO5X10PnoPfFmqishoUnu2rTM1Ywp53ocan91cDpbe1+HwRobG+HzQe5ntMWDcKVB4+Y1MXSKonul6IkEE6c0ADmUYS/oEgj/SDqQZOqwV/IAL/KUc4BW8rwdrMnOu5ktLlbwKBgQC2/p+/N2ETAksn3jyOmJL9TfGSAENaEvbW/RwSbGmcS+vsO4XVCuFjuy82sc7TLUT5ETYvnb0EfEE2PZo8SKlYKLX0E8nuYlxO0+GA7L7YRTYB/+Yh5OhPdXrIzLGmNeKvlNFi+7LkPQYFqvbRXdAOiGh2GU4JnDFST9TDAmfJMQKBgQCqMj/sCXi/fvfjoU/CneGXrZtzQv8NU0z7XbUUcbePZXQuijZx7P9phgVEIQEqmLPD2M5i2LrU8ZOPraXORAuf6qobf0XFS/ULdnBIFMhPDBOJVTrmPIoup++YQs/n+cxQnm0sqrjTJ6z+XZpPPRuqgBLxtIyrGIL2NQU8azgCmwKBgB5auDbowcJ8c+kGFTd2GZPI9RRJ1OJBGQ7qZ1irAQ27DkReT0Hajy6S4y8IaY1gCfvingFiAfOXhN2xdD8hsm18O0pkae+QA03UGajgHHwVZvMH9q2SofJkQ1zBj4pJj/OeFOl9ZNw1PyV7iXuLOzLd763/XpF7hjboJRu/72VRAoGALMCI3DJl8xXcH6O3J4FovyyVQBoupLPKM6aoj7fCGzE6Zayc2CI/O/GgMQ0jxkJpnIw7rMiMMs4vzi7kJwU3Qf0fw12+rZ9LthNNzMJMh+Cf+iRi90nHLw7O++IjI61ot/dHGZb0dZEd9XE7+4suboZGIbsZqYEMQzclL6ilwgM=";
+    public static final String PRIVATE_KEY = "";
 
     /**
      * 发起方商编
@@ -36,25 +36,25 @@ public class YeePayConfig {
     /**
      * 支付通知地址
      */
-    public static final String PAY_NOTIFY_URL = "http://zhongcangpay.solisoli.top/api/payNotify/yeePay";
+    public static final String PAY_NOTIFY_URL = "http://xxx/api/payNotify/yeePay";
 
     /**
      * 清算通知地址
      * 该回调地址通过下单接口的csUrl参数上送，接到清算回调的订单才可以进行分账（实时分账也可以将清算回调处理的进程作为分账触发）
      */
-    public static final String CS_NOTIFY_URL = "http://zhongcangpay.solisoli.top/api/payNotify/yeePay/divideApply";
+    public static final String CS_NOTIFY_URL = "http://xxx/api/payNotify/yeePay/divideApply";
 
     /**
      * 开通钱包通知地址
-     * http://zhongcangpay.solisoli.top/api/userSetting/yeePay/notify
+     * http://xxx/api/userSetting/yeePay/notify
      */
-    public static final String WALLET_NOTIFY_URL = "http://zhongcangpay.solisoli.top/api/userSetting/yeePay/wallet/notify";
+    public static final String WALLET_NOTIFY_URL = "http://xxx/api/userSetting/yeePay/wallet/notify";
 
     /**
      * 商户入网通知地址-小微(个人)
      * 入网结果通知:https://open.yeepay.com/docs/products/ptssfk/spis/5f3de63c20289f001ba82527
      */
-    public static final String MICRO_NOTIFY_URL = "http://zhongcangpay.solisoli.top/api/userSetting/yeePay/account/notify";
+    public static final String MICRO_NOTIFY_URL = "http://xxx/api/userSetting/yeePay/account/notify";
 
     public static final String SUCCESS = "SUCCESS";
 

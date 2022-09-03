8aa799d762bce9f311f78843af5e148c591f86c5
wangzehui <18500507445@163.com>
2022-09-03 20:56:59 +0800

    修改redis

--- nova-redis/src/test/java/com/nova/redis/RedisApplicationTest.java
+++ nova-redis/src/test/java/com/nova/redis/RedisApplicationTest.java
@@ -1,13 +1,28 @@
 package com.nova.redis;
 
+import cn.hutool.core.util.ObjectUtil;
+import com.nova.redis.core.RedisService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedisApplicationTest {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private RedisService redisService;
+
+    public static void main(String[] args) {
+
     }
 
+    @Test
+    public void testRedis() {
+        String key = "nova-redis-key";
+        redisService.set(key, "1", 5 * 60L);
+        Object o = redisService.get(key);
+        if (ObjectUtil.isNotNull(o)) {
+            System.out.println(o.toString());
+        }
+    }
 }

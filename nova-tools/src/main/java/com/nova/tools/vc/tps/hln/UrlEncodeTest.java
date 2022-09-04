f2fe3539574704ece8b42fb8cb8175291b1d2e2f
wangzehui <18500507445@163.com>
2022-09-04 20:43:11 +0800

    删除老代码

--- nova-tools/src/main/java/com/nova/tools/vc/tps/hln/UrlEncodeTest.java
+++ nova-tools/src/main/java/com/nova/tools/vc/tps/hln/UrlEncodeTest.java
@@ -10,21 +10,15 @@ import java.net.URLEncoder;
  */
 
 public class UrlEncodeTest {
-    public static void main (String[] args){
-       String s = "0300000400901001";
 
-
+    public static void main(String[] args) {
+        String s = "0300000400901001";
         try {
             String encode = URLEncoder.encode(s, "UTF-8");
-
-
             System.out.println(encode);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-
-
-
 
 
     }

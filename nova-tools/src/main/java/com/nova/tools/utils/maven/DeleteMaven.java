ac4fcdf3e3739fdd573dd55504f71c69a8e87531
wangzehui <18500507445@163.com>
2022-09-07 20:20:52 +0800

    撤销改动

--- nova-tools/src/main/java/com/nova/tools/utils/maven/DeleteMaven.java
+++ nova-tools/src/main/java/com/nova/tools/utils/maven/DeleteMaven.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * 删除本地maven仓库中的空文件夹
  * 删除本地maven仓库中含有后缀为.lastUpdated的文件的文件夹
  * 删除本地maven仓库中后缀为.jar-in-progress的文件的文件夹
- * @Author: wangzehui
+ * @Author: wangzehui1
  * @Date: 2022/8/6 13:17
  */
 public class DeleteMaven {

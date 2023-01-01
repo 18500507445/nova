57fd2b9e3a7d9041a4947739678a4b4821a5277a
wangzehui <18500507445@163.com>
2023-01-01 11:35:02 +0800

    整理路径

---
+++ nova-spring-mvc/src/main/java/com/nova/springmvc/config/MvcConfiguration.java
@@ -0,0 +1,13 @@
+package com.nova.springmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: spring-mvc配置文件
+ * @author: wzh
+ * @date: 2023/1/1 11:22
+ */
+@Configuration
+public class MvcConfiguration {
+
+}

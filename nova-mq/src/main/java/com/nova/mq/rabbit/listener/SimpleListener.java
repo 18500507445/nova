2dd3fcda3b09b945e623b0eb381dc1d2bdb10f6a
wangzehui <18500507445@163.com>
2023-01-06 17:45:34 +0800

    mq简单模式完结

--- nova-mq/src/main/java/com/nova/mq/rabbit/listener/SimpleListener.java
+++ nova-mq/src/main/java/com/nova/mq/rabbit/listener/SimpleListener.java
@@ -109,9 +109,8 @@ public class SimpleListener {
     @RabbitListener(queuesToDeclare = @Queue(RabbitConstants.QUEUE_SIMPLE_SEVEN), ackMode = "MANUAL")
     public void seven(Message message, Channel channel) {
         long tag = message.getMessageProperties().getDeliveryTag();
-        String body = null;
         try {
-            body = new String(message.getBody());
+            String body = new String(message.getBody());
             System.out.println("简单模式seven消息：" + body);
         } finally {
             /**
@@ -122,13 +121,10 @@ public class SimpleListener {
 
             /**
              * 否定消息，参数说明：
-             * boolean multiple：是否批处理，当该参数为 true 时，则可以一次性确认 deliveryTag 小于等于传入值的所有消息。
-             * boolean requeue：如果 requeue 参数设置为 true，
-             * 则 RabbitMQ 会重新将这条消息存入队列，以便发送给下一个订阅的消费者；
-             * 如果 requeue 参数设置为 false，则 RabbitMQ 立即会还把消息从队列中移除，
-             * 而不会把它发送给新的消费者。
+             * boolean requeue：如果 requeue 参数设置为 true，RabbitMQ 会重新将这条消息存入队列，以便发送给下一个订阅的消费者；
+             * 反之设置为false，则 RabbitMQ 立即会还把消息从队列中移除，而不会把它发送给新的消费者。
              */
-            //channel.basicNack(deliveryTag, true, false);
+            //channel.basicNack(tag, false, true);
 
             /**
              * 拒绝消息，参数说明：
@@ -137,7 +133,7 @@ public class SimpleListener {
              * 如果 requeue 参数设置为 false，则 RabbitMQ 立即会还把消息从队列中移除，
              * 而不会把它发送给新的消费者。
              */
-            //channel.basicReject(deliveryTag, true);
+            //channel.basicReject(tag, true);
         }
     }
 

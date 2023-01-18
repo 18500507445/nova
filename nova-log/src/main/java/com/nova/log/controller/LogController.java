a1d0397c6d06ae0b73977b79ead9f7b6d0dc72f4
wangzehui <18500507445@163.com>
2023-01-18 09:49:42 +0800

    修改笔记

--- nova-log/src/main/java/com/nova/log/controller/LogController.java
+++ nova-log/src/main/java/com/nova/log/controller/LogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description:
+ * @description: 异步logController
  * @author: wzh
  * @date: 2022/11/19 17:19
  */
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/")
-public class DemoController extends BaseController {
+public class LogController extends BaseController {
 
     /**
      * log
--- nova-rpc/rpcNote.md
+++ nova-rpc/rpcNote.md
@@ -1,6 +1,11 @@
 ## nova-rpc
 
 ### 简介:
+* rpc介绍
 
-* rpc框架
-
+### 组织结构
+```
+nova-rpc
+├── manual -- 手写rpc
+└── demo -- 
+```

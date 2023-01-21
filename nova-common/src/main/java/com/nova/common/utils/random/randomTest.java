d1d742bdcc24b44b6d074b36fb1aec2b03252734
wangzehui <18500507445@163.com>
2023-01-21 20:36:21 +0800

    红包算法

--- nova-common/src/main/java/com/nova/common/utils/random/randomTest.java
+++ nova-common/src/main/java/com/nova/common/utils/random/randomTest.java
@@ -4,6 +4,8 @@ package com.nova.common.utils.random;
 import cn.hutool.core.lang.WeightRandom;
 import cn.hutool.core.util.StrUtil;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -64,8 +66,38 @@ public class randomTest {
         return "";
     }
 
+    static class RedPackage {
+        int remainSize;
+        BigDecimal remainMoney;
+    }
+
+    public static BigDecimal getRandomMoney(RedPackage _redPackage) {
+        // remainSize 剩余的红包数量 remainMoney 剩余的钱
+        if (_redPackage.remainSize == 1) {
+            _redPackage.remainSize--;
+            return _redPackage.remainMoney.setScale(2, RoundingMode.DOWN);
+        }
+
+        BigDecimal random = BigDecimal.valueOf(Math.random());
+        BigDecimal min = BigDecimal.valueOf(0.01);
+
+        BigDecimal halfRemainSize = BigDecimal.valueOf(_redPackage.remainSize).divide(new BigDecimal(2), RoundingMode.UP);
+        BigDecimal max1 = _redPackage.remainMoney.divide(halfRemainSize, RoundingMode.DOWN);
+        BigDecimal minRemainAmount = min.multiply(BigDecimal.valueOf(_redPackage.remainSize - 1)).setScale(2, RoundingMode.DOWN);
+        BigDecimal max2 = _redPackage.remainMoney.subtract(minRemainAmount);
+        BigDecimal max = (max1.compareTo(max2) < 0) ? max1 : max2;
+
+        BigDecimal money = random.multiply(max).setScale(2, RoundingMode.DOWN);
+        money = money.compareTo(min) < 0 ? min : money;
+
+        _redPackage.remainSize--;
+        _redPackage.remainMoney = _redPackage.remainMoney.subtract(money).setScale(2, RoundingMode.DOWN);
+        return money;
+    }
+
     /**
      * 方式二 参照huTool WeightRandom
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -121,6 +153,18 @@ public class randomTest {
         System.out.println("sum6:" + sum6);
         System.out.println("sum7:" + sum7);
         System.out.println("sum8:" + sum8);
+
+        for (int i = 0; i < 50; i++) {
+            RedPackage moneyPackage = new RedPackage();
+            moneyPackage.remainMoney = BigDecimal.valueOf(100);
+            moneyPackage.remainSize = 5;
+
+            while (moneyPackage.remainSize != 0) {
+                System.out.print(getRandomMoney(moneyPackage)  + "   ");
+            }
+
+            System.out.println();
+        }
     }
 
 

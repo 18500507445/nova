2af44a41d1f29ada4086aa645c1d3ab5a2fc3b23
wangzehui <18500507445@163.com>
2023-02-13 21:34:58 +0800

    effective java第二章 第一节

---
+++ nova-ebook/src/main/java/com/nova/ebook/effectivejava/chapter2/section1/BlackCat.java
@@ -0,0 +1,28 @@
+package com.nova.ebook.effectivejava.chapter2.section1;
+
+import java.util.Objects;
+
+/**
+ * @description:
+ * @author: wzh
+ * @date: 2023/2/13 20:05
+ */
+public class BlackCat extends Cat {
+
+    public BlackCat() {
+        super();
+        super.setColour("black");
+    }
+
+    @Override
+    public boolean equals(Object param) {
+        if (param == this) {
+            return true;
+        }
+        if (param instanceof BlackCat) {
+            BlackCat cat = (BlackCat) param;
+            return Objects.equals(getId(), cat.getId()) && Objects.equals(getName(), cat.getName());
+        }
+        return false;
+    }
+}

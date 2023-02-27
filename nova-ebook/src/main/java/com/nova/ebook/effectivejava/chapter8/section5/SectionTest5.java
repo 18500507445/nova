79a7d90fdb52034c27a740097fae5464b521f57e
wangzehui <18500507445@163.com>
2023-02-27 15:05:29 +0800

    effective java 第八章

--- nova-ebook/src/main/java/com/nova/ebook/effectivejava/chapter8/section5/SectionTest5.java
+++ nova-ebook/src/main/java/com/nova/ebook/effectivejava/chapter8/section5/SectionTest5.java
@@ -6,5 +6,13 @@ package com.nova.ebook.effectivejava.chapter8.section5;
  * @date: 2023/2/22 14:20
  */
 class SectionTest5 {
-    
+
+    /**
+     * @param c collection containing elements to be added to this list
+     * @return <tt>true</tt> if this list changed as a result of the call
+     * @throws NullPointerException if the specified collection is null
+     */
+    public boolean addAll() {
+        return true;
+    }
 }
--- nova-ebook/src/main/java/com/nova/ebook/effectivejava/chapter8/section6/SectionTest6.java
+++ nova-ebook/src/main/java/com/nova/ebook/effectivejava/chapter8/section6/SectionTest6.java
@@ -1,10 +1,29 @@
 package com.nova.ebook.effectivejava.chapter8.section6;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @description:
  * @author: wzh
  * @date: 2023/2/22 14:20
  */
 class SectionTest6 {
-    
+
+    @Test
+    public void demoA() {
+        //空指针异常打印
+        get(null, 1);
+    }
+
+    @Test
+    public void demoB() {
+        int[] iss = new int[1];
+        //数组下标砰界异常
+        get(iss, 3);
+    }
+
+    public static int get(int[] intArray, int index) {
+        return intArray[index];
+
+    }
 }

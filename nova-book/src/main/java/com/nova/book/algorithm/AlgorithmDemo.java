712df8f3d92c652b409cb93103db94df12214334
wangzehui <18500507445@163.com>
2023-03-05 10:42:38 +0800

    整理目录

---
+++ nova-book/src/main/java/com/nova/book/algorithm/AlgorithmDemo.java
@@ -0,0 +1,9 @@
+package com.nova.book.algorithm;
+
+/**
+ * @description:
+ * @author: wzh
+ * @date: 2023/3/5 10:42
+ */
+public class AlgorithmDemo {
+}
---
+++ nova-book/src/main/java/com/nova/book/jvm/DemoJvm.java
@@ -0,0 +1,9 @@
+package com.nova.book.jvm;
+
+/**
+ * @description:
+ * @author: wzh
+ * @date: 2023/3/5 10:40
+ */
+public class DemoJvm {
+}

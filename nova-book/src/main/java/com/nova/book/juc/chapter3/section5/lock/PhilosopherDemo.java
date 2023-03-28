ad8f3f7bb4f3bc860d07539955467c95b519a969
wangzehui <18500507445@163.com>
2023-03-28 22:06:26 +0800

    哲学筷子吃饭问题

---
+++ nova-book/src/main/java/com/nova/book/juc/chapter3/section5/lock/PhilosopherDemo.java
@@ -0,0 +1,79 @@
+package com.nova.book.juc.chapter3.section5.lock;
+
+import com.nova.common.utils.thread.Threads;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @description: 哲学筷子吃饭问题
+ * @author: wzh
+ * @date: 2023/3/28 22:01
+ */
+class PhilosopherDemo {
+
+    public static void main(String[] args) {
+        Chopstick c1 = new Chopstick("1");
+        Chopstick c2 = new Chopstick("2");
+        Chopstick c3 = new Chopstick("3");
+        Chopstick c4 = new Chopstick("4");
+        Chopstick c5 = new Chopstick("5");
+        new Philosopher("苏格拉底", c1, c2).start();
+        new Philosopher("柏拉图", c2, c3).start();
+        new Philosopher("亚里士多德", c3, c4).start();
+        new Philosopher("赫拉克利特", c4, c5).start();
+        new Philosopher("阿基米德", c5, c1).start();
+    }
+}
+
+
+@Slf4j(topic = "Philosopher")
+class Philosopher extends Thread {
+    Chopstick left;
+    Chopstick right;
+
+    public Philosopher(String name, Chopstick left, Chopstick right) {
+        super(name);
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            //　尝试获得左手筷子
+            if (left.tryLock()) {
+                try {
+                    // 尝试获得右手筷子
+                    if (right.tryLock()) {
+                        try {
+                            eat();
+                        } finally {
+                            right.unlock();
+                        }
+                    }
+                } finally {
+                    left.unlock(); // 释放自己手里的筷子
+                }
+            }
+        }
+    }
+
+    private void eat() {
+        log.debug("eating...");
+        Threads.sleep(500);
+    }
+}
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+class Chopstick extends ReentrantLock {
+    String name;
+
+    public Chopstick(String name) {
+        this.name = name;
+    }
+
+}

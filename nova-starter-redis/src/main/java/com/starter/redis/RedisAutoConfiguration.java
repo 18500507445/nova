4931cf848f8985a9550316686266466e9adc8c3d
wangzehui <18500507445@163.com>
2023-04-22 20:28:04 +0800

    自定义redis-starter

---
+++ nova-starter-redis/src/main/java/com/starter/redis/RedisAutoConfiguration.java
@@ -0,0 +1,39 @@
+package com.starter.redis;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @description: redis自动配置类
+ * @author: wzh
+ * @date: 2023/4/22 20:08
+ */
+@AutoConfiguration
+public class RedisAutoConfiguration {
+
+    /**
+     * 创建 RedisTemplate Bean，使用 JSON 序列化方式
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        // 创建 RedisTemplate 对象
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 设置 RedisConnection 工厂。😈 它就是实现多种 Java Redis 客户端接入的秘密工厂。
+        template.setConnectionFactory(factory);
+        // 使用 String 序列化方式，序列化 KEY 。
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 使用 JSON 序列化方式（库是 Jackson ），序列化 VALUE 。
+        template.setValueSerializer(RedisSerializer.json());
+        template.setHashValueSerializer(RedisSerializer.json());
+        return template;
+    }
+
+    @Bean(name = "redisService")
+    public RedisService redisService() {
+        return new RedisService();
+    }
+}
--- nova-tools/src/test/java/com/nova/tools/ToolsApplicationTests.java
+++ nova-tools/src/test/java/com/nova/tools/ToolsApplicationTests.java
@@ -1,16 +1,8 @@
 package com.nova.tools;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ToolsApplicationTests {
-
-    @Test
-    void contextLoads() {
-
-    }
-
-
 
 }

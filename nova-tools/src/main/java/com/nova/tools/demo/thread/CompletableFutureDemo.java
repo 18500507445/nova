0799ae18a3e81d5cbd679c6c94a311d1b8b01e07
wangzehui <18500507445@163.com>
2023-05-22 17:50:26 +0800

    提交测试

--- nova-tools/src/main/java/com/nova/tools/demo/thread/CompletableFutureDemo.java
+++ nova-tools/src/main/java/com/nova/tools/demo/thread/CompletableFutureDemo.java
@@ -1,19 +1,23 @@
 package com.nova.tools.demo.thread;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONUtil;
+import com.nova.common.utils.thread.Threads;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -93,6 +97,64 @@ class CompletableFutureDemo {
         }
         return i;
     }
+
+
+    private static final TimeInterval TIMER = DateUtil.timer();
+
+    /**
+     * 只要有一个异步线程完成就立刻返回结果
+     */
+    @Test
+    public void testSync() {
+        List<Integer> urlList = CollUtil.newArrayList(1, 2, 3, 4, 5, 6);
+        List<Integer> randomList = RandomUtil.randomEleList(urlList, 3);
+
+        Object result = CompletableFuture.anyOf(randomList.stream().map(integer -> CompletableFuture.supplyAsync(() -> {
+            Threads.sleep(integer * 1000);
+            return integer;
+        })).toArray(CompletableFuture[]::new)).join();
+
+        System.out.println("耗时：" + TIMER.interval());
+        System.out.println(result);
+
+    }
+
+    @Test
+    public void testC() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        //supplyAsync 构造一个CompletableFuture
+        //CompletableFuture.completedFuture() 构造一个已完成的future
+        //thenApply 相当于回调函数
+        List<CompletableFuture> completableFutures = list.stream().map(e -> CompletableFuture.supplyAsync(() -> {
+            //异步任务执行 模拟耗时任务
+            Threads.sleep(Convert.toInt(e) * 1000);
+            integers.add(Convert.toInt(e));
+            return e;
+        }).thenApply(String::toUpperCase)).collect(Collectors.toList());
+        //阻塞主线程直到所有全部任务完成
+        CompletableFuture.anyOf(completableFutures.toArray(new CompletableFuture[0])).join();
+
+        System.out.println(TIMER.interval());
+        System.out.println(JSONUtil.toJsonStr(integers));
+
+        completableFutures.forEach(e -> {
+            try {
+                //获得执行结果
+                System.out.println("执行结果" + e.get());
+            } catch (InterruptedException | ExecutionException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+    }
+
+
 }
 
 

a8d98507d837b7c549ff56aa72695fc500ae3c06
wangzehui <18500507445@163.com>
2023-08-03 17:00:04 +0800

    add：apache ab测试demo

--- nova-tools/src/main/java/com/nova/tools/demo/DemoController.java
+++ nova-tools/src/main/java/com/nova/tools/demo/DemoController.java
@@ -1,8 +1,11 @@
 package com.nova.tools.demo;
 
+import cn.hutool.core.thread.ThreadUtil;
+import com.nova.common.core.model.result.AjaxResult;
 import com.starter.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +22,20 @@ public class DemoController {
 
     private final RedisService redisService;
 
+    /**
+     * apache ab测试
+     * 查看版本：apachectl -v
+     * 测试连接：ab -n 请求数 -c 并发数 URL
+     * ab工具常用参数：
+     * -n：总共的请求执行数，缺省是1；
+     * -c： 并发数，缺省是1；
+     * -t：测试所进行的总时间，秒为单位，缺省50000s
+     * -p：POST时的数据文件
+     * -w: 以HTML表的格式输出结果
+     */
+    @GetMapping("abTest")
+    public AjaxResult abTest() {
+        ThreadUtil.sleep(300);
+        return AjaxResult.success();
+    }
 }

ebead0f2e9e71f2a1742eccf794b3c92068141e4
wangzehui <18500507445@163.com>
2023-09-28 10:53:21 +0800

    update：整理pom

--- nova-tools/src/main/java/com/nova/tools/utils/guava/StringUtilsTest.java
+++ nova-tools/src/main/java/com/nova/tools/utils/guava/StringUtilsTest.java
@@ -1,7 +1,6 @@
 package com.nova.tools.utils.guava;
 
 import com.google.common.base.CharMatcher;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,48 +9,6 @@ import org.junit.jupiter.api.Test;
  * @date: 2022/10/13 18:26
  */
 class StringUtilsTest {
-
-    /**
-     * 12345678911
-     * 脱敏
-     * 123****8911
-     */
-    @Test
-    public void test3() {
-        String str = "12345678911";
-        // 返回某个字符串左边的几个字符
-
-        // 123
-        String left = StringUtils.left(str, 3);
-        // 8911
-        String right = StringUtils.right(str, 4);
-        System.err.println(left + "****" + right);
-        // rightPad：如果left长度<7,那么就在右边用*填充到7个长度
-        String padResult = StringUtils.rightPad(left, 7, '*');
-        System.err.println(padResult + right);
-    }
-
-    /**
-     * 判断null/""
-     */
-    @Test
-    public void test2() {
-        String str = "   ";
-        System.err.println(StringUtils.isEmpty(str));
-        // 反过来
-        System.err.println(StringUtils.isNotEmpty(str));
-    }
-
-    /**
-     * 判断null/""/"   "
-     */
-    @Test
-    public void test1() {
-        String str = "   ";
-        System.err.println(StringUtils.isBlank(str));
-        // 反过来
-        System.err.println(StringUtils.isNotBlank(str));
-    }
 
     /**
      * 字符串的处理

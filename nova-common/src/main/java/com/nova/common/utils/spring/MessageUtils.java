4420321301e6fe5eb4f5c29137c283de4887fc75
wzh <18500507445@163.com>
2024-05-08 15:27:13 +0800

    update：修改工具类

--- nova-common/src/main/java/com/nova/common/utils/spring/MessageUtils.java
+++ nova-common/src/main/java/com/nova/common/utils/spring/MessageUtils.java
@@ -7,7 +7,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 /**
- * 获取i18n资源文件
+ * @description: spring国际化，获取i18n资源文件
  * @author wzh
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)

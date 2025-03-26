5236c845a17d9a69b8aa1e8ff3ca98670fbcd4b3
wzh <18500507445@163.com>
2025-03-26 17:47:37 +0800

    feature：整理代码

--- nova-login/nova-login-sa/src/main/java/com/nova/login/sa/SaTokenApplication.java
+++ nova-login/nova-login-sa/src/main/java/com/nova/login/sa/SaTokenApplication.java
@@ -16,6 +16,7 @@ public class SaTokenApplication {
     public static void main(String[] args) {
         SpringApplication.run(SaTokenApplication.class, args);
         System.err.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
+        System.err.println("官网：https://sa-token.cc/doc.html#/");
     }
 
 }

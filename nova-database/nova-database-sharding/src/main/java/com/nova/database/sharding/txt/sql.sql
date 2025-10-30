ab182a81b8aa1f56935e4fb86d57d625bd333f09
wzh <18500507445@163.com>
2025-10-30 11:38:51 +0800

    feature：sharding 分库分表

---
+++ nova-database/nova-database-sharding/src/main/java/com/nova/database/sharding/txt/sql.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `t_order_0` (
+  `order_id` bigint NOT NULL,
+  `user_name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `t_order_1` (
+  `order_id` bigint NOT NULL,
+  `user_name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `t_order_2` (
+  `order_id` bigint NOT NULL,
+  `user_name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;

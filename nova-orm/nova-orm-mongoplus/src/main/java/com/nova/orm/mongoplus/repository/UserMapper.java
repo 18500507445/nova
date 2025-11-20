b412c78bb7477c12dc9fb722b134335363d7703a
wzh <18500507445@163.com>
2025-11-20 10:40:43 +0800

    feature：修改文档

--- nova-orm/nova-orm-mongoplus/src/main/java/com/nova/orm/mongoplus/repository/UserMapper.java
+++ nova-orm/nova-orm-mongoplus/src/main/java/com/nova/orm/mongoplus/repository/UserMapper.java
@@ -1,9 +1,11 @@
 package com.nova.orm.mongoplus.repository;
 
+import com.mongoplus.annotation.mapper.Mongo;
 import com.mongoplus.mapper.MongoMapper;
 
 // mapper接口需要继承`MongoMapper`接口，并传入所属实体泛型
 // solon需要在此处标注 @Mongo注解
+@Mongo
 public interface UserMapper extends MongoMapper<User> {
 
 }
